c1300c8208094d625c5dc57c882dd2b45e6253e8
tomyjuly <taquias@hotmail.com>
2023-03-01 11:00:37 -0300

    first com

---
+++ js/function.js
@@ -0,0 +1,31 @@
+
+/**document.addEventListener('mouseover', function1);*/
+
+    function colorrojo() 
+    {
+        document.getElementById('titulo').style.color = 'red';
+    }
+
+   /** document.addEventListener("mouseout", function2);*/
+
+    function colornegro() 
+    {
+        document.getElementById('titulo').style.color = 'black';
+    }
+
+
+    function agrandarfoto() 
+    {
+        document.getElementById('cvfoto').style.height= '200px';
+        document.getElementById('cvfoto').style.width= '200px';
+    }
+
+   /** document.addEventListener("mouseout", function2);*/
+
+    function achicarfoto() 
+    {
+       document.getElementById('cvfoto').style.height= '100px';
+       document.getElementById('cvfoto').style.width= '100px';
+    }
+
+
